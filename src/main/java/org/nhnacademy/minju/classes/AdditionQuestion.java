627a226bedd4fd9bef42fee9f6ab3ce8394e0efc
minm063 <jangmin063@gmail.com>
2023-09-17 18:54:57 -0700

    feat : Exercise6

---
+++ src/main/java/org/nhnacademy/minju/classes/AdditionQuestion.java
@@ -0,0 +1,20 @@
+package org.nhnacademy.minju.classes;
+
+public class AdditionQuestion {
+
+    private int a, b;  // The numbers in the problem.
+
+    public AdditionQuestion() { // constructor
+        a = (int)(Math.random() * 50 + 1);
+        b = (int)(Math.random() * 50);
+    }
+
+    public String getQuestion() {
+        return "What is " + a + " + " + b + " ?";
+    }
+
+    public int getCorrectAnswer() {
+        return a + b;
+    }
+
+}
