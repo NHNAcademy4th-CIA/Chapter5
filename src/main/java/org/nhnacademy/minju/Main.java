4d696ea660bb14a97f1185b29eb7629167fe352a
minm063 <jangmin063@gmail.com>
2023-09-17 18:55:46 -0700

    feat : Exercise7

---
+++ src/main/java/org/nhnacademy/minju/Main.java
@@ -0,0 +1,13 @@
+package org.nhnacademy.minju;
+
+public class Main {
+    public static void main(String[] args) {
+//        Exercise1.exercise1();
+//        Exercise2.exercise2();
+//        Exercise3.exercise3();
+//        Exercise4.exercise4();
+        Exercise5.exercise5();
+//        Exercise6.exercise6();
+//        Exercise7.exercise7();
+    }
+}
